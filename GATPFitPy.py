353c116d1380bb16afec1e1ee9a420f15d7c7615
Gregory Magoon <gmagoon@mit.edu>
2009-10-22 14:29:42 -0400

    fixing to handle case where cpInf = cp0; note that in this case, the Wilhoit form is only able to predict a constant heat capacity; so we would have trouble fitting Cp for species like O atom where intermediate values do not equal limit values

--- GATPFitPy.py
+++ GATPFitPy.py
@@ -37,7 +37,8 @@ def CpFit(atoms, rotors, linearity, t, cp, fixed, weighting):
     #if we are using fixed tint, set tint equal to Tintg and do not allow tint to float
     if(fixed == 1):
     	(b1, b2, b3, b4, b5, b6, b7, b8, b9, b10) = Wilhoit2NASA(cp0, cpInf, B, a0, a1, a2, a3, Tmin, Tmax, Tintg, weighting)
-      TintOpt_objFun(Tintg, cp0, cpInf, B, a0, a1, a2, a3, Tmin, Tmax, weighting) #to print the objective function value
+        #the following prints the optimization function value
+        TintOpt_objFun(Tintg, cp0, cpInf, B, a0, a1, a2, a3, Tmin, Tmax, weighting)
     	tint = Tintg
     else:
     	(b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, tint) = Wilhoit2NASA_TintOpt(cp0, cpInf, B, a0, a1, a2, a3, Tmin, Tmax, Tintg, weighting)
@@ -133,11 +134,11 @@ def WilhoitFit(t, cp, cp0, cpInf, B):
     b = scipy.zeros([m,1])
     for i in range(m):
         y = t[i]/(t[i]+B)
-        A[i,0] = y*y*(y-1)
-        A[i,1] = A[i,0]*y
-        A[i,2] = A[i,1]*y
-        A[i,3] = A[i,2]*y
-        b[i] = (cp[i]-cp0)/(cpInf-cp0) - y*y
+        A[i,0] = y*y*(y-1)*(cpInf-cp0)
+        A[i,1] = A[i,0]*y*(cpInf-cp0)
+        A[i,2] = A[i,1]*y*(cpInf-cp0)
+        A[i,3] = A[i,2]*y*(cpInf-cp0)
+        b[i] = cp[i]-cp0 - y*y*(cpInf-cp0)
     #solve least squares problem A*x = b
     #http://docs.scipy.org/doc/scipy/reference/tutorial/linalg.html#solving-linear-least-squares-problems-and-pseudo-inverses
     x,resid,rank,sigma = linalg.lstsq(A,b)
